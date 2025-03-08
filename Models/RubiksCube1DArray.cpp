2d68064658728b16f65081613c00a3eda67db2e9
GITHUBTANMAY2003 <tanmayhost@gmail.com>
2025-03-08 19:13:29 +0530

    Added Rotate Function

--- Models/RubiksCube1DArray.cpp
+++ Models/RubiksCube1DArray.cpp
@@ -1,8 +1,19 @@
 #include "RubiksCubeAbstarct.h"
-class RubiksCube1dArray : public RubiksCube {
+class RubiksCube1DArray : public RubiksCube {
   private:
       static inline int getFlattenedIndex(int faceIndex, int rowIndex, int colIndex) {
           return (faceIndex * 9) + (rowIndex * 3) + colIndex;
       }
+      void rotate(int faceIndex){
+        char temp_vec[9]={};
+        for(int i=0;i<3;i++){
+          for(int j=0;j<3;j++){
+            temp_vec[i*3+j]=cube[getFlattenedIndex(faceIndex,i,j)];
+          }
+        }
+        
+
+      }
     }
+
   
