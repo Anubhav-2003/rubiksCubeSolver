f161c7841ec8570b43fdf40833f86fdcaa402cc1
Anubhav Banerjee <banerjeeanubhav03@gmail.com>
2025-03-08 17:32:02 +0530

    Added more definations to the abstract class

--- RubiksCubeAbstarct.h
+++ RubiksCubeAbstarct.h
@@ -27,9 +27,55 @@ class RubiksCube {
 
         virtual bool isSolved() const = 0;
 
-        static string getMove(MOVE index);
+        static string getMove(MOVE move);
 
         void printCube() const;
 
         vector<MOVE> ShuffleCube(int iterations);
+
+        RubiksCube &move(MOVE move);
+
+        RubiksCube &invert(MOVE move);
+
+        virtual RubiksCube &f() = 0;
+
+        virtual RubiksCube &l() = 0;
+
+        virtual RubiksCube &lPrime() = 0;
+
+        virtual RubiksCube &l2() = 0;
+
+        virtual RubiksCube &bPrime() = 0;
+
+        virtual RubiksCube &b2() = 0;
+
+        virtual RubiksCube &r() = 0;
+
+        virtual RubiksCube &d() = 0;
+
+        virtual RubiksCube &uPrime() = 0;
+
+        virtual RubiksCube &u2() = 0;
+
+        virtual RubiksCube &rPrime() = 0;
+
+        virtual RubiksCube &r2() = 0;
+
+        virtual RubiksCube &b() = 0;
+
+        virtual RubiksCube &dPrime() = 0;
+
+        virtual RubiksCube &d2() = 0;
+
+        virtual RubiksCube &fPrime() = 0;
+
+        virtual RubiksCube &f2() = 0;
+
+        virtual RubiksCube &u() = 0;
+
+        string getCornerColorCode(int ind) const;
+
+        int getCornerIndex(int ind) const;
+
+        int getCornerOrientation(int ind) const;
 }
