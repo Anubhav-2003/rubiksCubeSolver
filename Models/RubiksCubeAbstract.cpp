ffd6fbe2b6218254ea375b48232c38761a39f6a3
Anubhav Banerjee <banerjeeanubhav03@gmail.com>
2025-03-08 19:04:55 +0530

    Added print Functionality for generic Cube

--- Models/RubiksCubeAbstract.cpp
+++ Models/RubiksCubeAbstract.cpp
@@ -99,3 +99,92 @@ RubiksCube &RubiksCube::move(MOVE move) {
             return this->r2();
     }
 }
+
+RubiksCube &RubiksCube::invert(MOVE move) {
+    switch (move) {
+        case MOVE::L:
+            return this->lPrime();
+        case MOVE::LPRIME:
+            return this->l();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->rPrime();
+        case MOVE::RPRIME:
+            return this->r();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->uPrime();
+        case MOVE::UPRIME:
+            return this->u();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->dPrime();
+        case MOVE::DPRIME:
+            return this->d();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->fPrime();
+        case MOVE::FPRIME:
+            return this->f();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->bPrime();
+        case MOVE::BPRIME:
+            return this->b();
+        case MOVE::B2:
+            return this->b2();
+    }
+}
+
+void RubiksCube::printCube() const {
+    cout << "Rubik's Cube:\n\n";
+
+    for (int row = 0; row <= 2; row++) {
+        for (unsigned i = 0; i < 7; i++) cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::UP, row, col)) << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    for (int row = 0; row <= 2; row++) {
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::LEFT, row, col)) << " ";
+        }
+        cout << " ";
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::FRONT, row, col)) << " ";
+        }
+        cout << " ";
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::RIGHT, row, col)) << " ";
+        }
+        cout << " ";
+
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::BACK, row, col)) << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    for (int row = 0; row <= 2; row++) {
+        for (unsigned i = 0; i < 7; i++) cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            cout << getColorLetter(getColor(FACE::DOWN, row, col)) << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
