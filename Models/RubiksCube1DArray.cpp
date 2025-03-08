d95e1f22e8ac2fb1a5da9de57f8fccb3f571b484
GITHUBTANMAY2003 <tanmayhost@gmail.com>
2025-03-08 19:00:19 +0530

    Fixed Merge Conflicts

--- Models/RubiksCube1DArray.cpp
+++ Models/RubiksCube1DArray.cpp
@@ -1,1 +1,8 @@
 #include "RubiksCubeAbstarct.h"
+class RubiksCube1dArray : public RubiksCube {
+  private:
+      static inline int getFlattenedIndex(int faceIndex, int rowIndex, int colIndex) {
+          return (faceIndex * 9) + (rowIndex * 3) + colIndex;
+      }
+    }
+  
