cb3f924d8c6325766806f38da535790c203e3b51
Anubhav Banerjee <banerjeeanubhav03@gmail.com>
2025-06-01 11:02:02 +0530

    Added HeuristicEstimator

---
+++ Models/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,14 @@
+#ifndef RUBIKS_CUBE_SOLVER_PATTERN_DATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERN_DATABASE_H
+
+template<typename T>
+class HeuristicEstimator {
+private:
+
+public:
+    int calculateHeuristic(T cubeState) {
+        return 0;
+    }
+};
+
+#endif
